f337a21ed61ba0c5dc74115bf1831e362a1f6563
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-12-03 10:22:30 -0500

    added compileroptions

--- apps/sam-design-system-site/src/app/app.component.spec.ts
+++ apps/sam-design-system-site/src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ToastrModule } from 'ngx-toastr';
-import { SdsToastModule } from '@gsa-sam/components';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [RouterTestingModule, ToastrModule, SdsToastModule]
+      imports: [RouterTestingModule]
     }).compileComponents();
   }));
 
